e95c0f827760a45a914cfd4f9088d78c31ea1535
hm1984ir <hm1984ir@gmail.com>
2015-03-06 09:17:38 +0330

    X.5 A Tour of c++: Containers and Algorithms

---
+++ X5/Exercise5/Exercise5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+	string name;
+	int age;
+	
+	cin >> name >> age;
+	
+	cout << "His name is " << name <<" and he is " << age << " years old.";
+}
+
