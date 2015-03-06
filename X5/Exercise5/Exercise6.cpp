d9d563c3c9d93bb932dd0ede43fbd1286e08c735
hm1984ir <hm1984ir@gmail.com>
2015-03-06 23:50:51 +0330

    .

--- X5/Exercise5/Exercise6.cpp
+++ X5/Exercise5/Exercise6.cpp
@@ -7,10 +7,10 @@ using namespace std;
 class Person{
 	public:
 		Person(int age, string name) : _age{age},_name{name}{}
-		string Get_Name(){
+		string Get_Name() const{
 			return _name;
 		}
-		int Get_Age(){
+		int Get_Age() const{
 			return _age;
 		}
 	private:
@@ -18,34 +18,33 @@ class Person{
 		string _name;
 };
 
-ostream& operator<<(ostream& os, Person& p){
+ostream& operator<<(ostream& os, const Person& p){
 			return os << "Name : " << p.Get_Name() << " Age: " << p.Get_Age();
 		}
 
 class Society{
 	public:
 		Society(string title) : _title {title}{};
-		void Add_Person(Person& person){
-			_Peaple->push_back(person);
+		void Add_Person(const Person& person){
+			_People.push_back(person);
 		}
 		list<Person> Get_People(){
 			return _People;
 		}
+		
 	private:
-		list<Person>* _People = new list<Person>();
+		list<Person> _People;
 		string _title;
 };
 
 ostream& operator<<(ostream& os, Society& s){
-	for(auto& p:s){
+	for(const Person& p:s.Get_People()){
 		cout << p << "/n";
 	}
 }
 
 
 int main(){
-	Vector<Person> vp= new Vector<Person>();
-	
 	Person p(22, "hamid");
 	cout << p;
 }
