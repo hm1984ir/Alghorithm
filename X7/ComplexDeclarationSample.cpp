24b57fea59d965a8cb2f838a50c4aee24b7fb67d
hm1984ir <hm1984ir@gmail.com>
2015-03-08 23:17:52 +0330

    ComplexDeclarationSample

--- X7/ComplexDeclarationSample.cpp
+++ X7/ComplexDeclarationSample.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<vector>
 
 int sayHello(){
 	std::cout << "hello";
@@ -8,7 +9,18 @@ int(* p)() ={sayHello};
 
 int main(){
 	p();
+	
+	int *arr[5]{};
+	int i = 12;
+	arr[0] = &i;
+	
+	std::cout << *arr[0];
+	
+	int a[] = {1,2};
+	
+	std::cout << a[0];
 }
+		
 
 
 
