7c5185c4f089913ee045cf91ded7e48f7c4b2b58
hm1984ir <hm1984ir@gmail.com>
2015-03-13 16:03:03 +0330

    .

---
+++ X8/Excersice7.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+void swap(int* a, int* b){
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+
+void swap(int& a, int& b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+
+    int a,b;
+    a = 1;
+    b = 2;
+
+    swap(&a, &b);
+    swap(a, b);
+
+    cout << "a:" << a << " b:" << b;
+
+}
+
